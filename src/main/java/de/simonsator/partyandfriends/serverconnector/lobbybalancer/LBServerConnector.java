883a4f32ba7e71461e1f1131114742a6d299969f
Simon Brungs <00pflaume@gmail.com>
2017-09-09 21:05:56 +0200

    First upload

---
+++ src/main/java/de/simonsator/partyandfriends/serverconnector/lobbybalancer/LBServerConnector.java
@@ -0,0 +1,29 @@
+package de.simonsator.partyandfriends.serverconnector.lobbybalancer;
+
+import de.simonsator.partyandfriends.api.PAFExtension;
+import de.simonsator.partyandfriends.api.friends.ServerConnector;
+import de.simonsator.partyandfriends.friends.subcommands.Jump;
+import me.jaimemartz.lobbybalancer.manager.PlayerLocker;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+/**
+ * @author Simonsator
+ * @version 1.0.0 26.06.17
+ */
+public class LBServerConnector extends PAFExtension implements ServerConnector {
+	public void onEnable() {
+		Jump.setServerConnector(this);
+	}
+
+	public void connect(ProxiedPlayer pPlayer, ServerInfo pServerInfo) {
+		PlayerLocker.lock(pPlayer);
+		pPlayer.connect(pServerInfo);
+		PlayerLocker.unlock(pPlayer);
+	}
+
+	public void reload() {
+		onDisable();
+		onEnable();
+	}
+}
